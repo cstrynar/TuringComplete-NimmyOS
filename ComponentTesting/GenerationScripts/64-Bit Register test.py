08dc0a6af62f9c3eef3ceb3faa69bd350746f253
chrisstrynar <chrisstrynar@machine.build>
2024-07-26 13:28:38 -0400

    Added first component test generation script.

---
+++ ComponentTesting/GenerationScripts/64-Bit Register test.py
@@ -0,0 +1,33 @@
+import random
+import time
+
+startTime = time.time()
+
+outputDir = 'ComponentTesting/Output'
+outputFile = '64-Bit Register test.txt'
+
+numTests = 100
+minVal = 0
+maxVal = 2 ** 64 - 1
+
+
+lines = []
+
+for i in range(numTests):
+    randNum = random.randint(minVal, maxVal)
+    lines.append(f'set_input {randNum}\n')
+    lines.append('1\n')
+    lines.append(f'expect 0 {randNum}\n')
+
+lines.append('2\n')
+lines.append('expect 0 0\n')
+lines.append('2\n')
+lines.append('2\n')
+lines.append('2\n')
+
+with open(f'{outputDir}/{outputFile}', 'w+') as f:
+    f.writelines(lines)
+
+print(f'Lab program file generated. Outputted to {outputDir}/{outputFile}')
+print(f'Generation runtime: {time.time() - startTime}')
+
