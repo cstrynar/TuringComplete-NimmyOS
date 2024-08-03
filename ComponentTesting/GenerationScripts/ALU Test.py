07c87a42c0d899b720d3aa86a322f655831f8874
chrisstrynar <chrisstrynar@machine.build>
2024-08-03 12:52:39 -0400

    Added integer operation testing. Added low-value random testing.

--- ComponentTesting/GenerationScripts/ALU Test.py
+++ ComponentTesting/GenerationScripts/ALU Test.py
@@ -13,15 +13,24 @@ globalStartTime = time.time()
 
 outputDir = 'ComponentTesting/Output'
 
-numTests = 50  # this is the number of tests to run per operation
 # values passed into the input are in 2's complement notation
 minVal = -2**63
 maxVal = 2**63 - 1
 
-lines = ['# Bitwise Operations\n']
+# the min and max vals used for low-range testing (inclusive)
+lowRangeMinVal = -100
+lowRangeMaxVal = 100
+
+edgeCaseValues = [0, 1, -1, minVal, maxVal]  # these are the values used in checking edge cases.
+                                             # we test every combination of the values in this list for the edge case tests
+numRandomTests = 25                          # this is the number of random tests to run for each value-range for each operation.
+
+global lines
+lines = []
 
 
 def Generate1InputOpcodeTest(input1: int, output: int):
+    global lines
     lines.append(f'set_input {input1}\n')
     lines.append('SaveInput1\n')
     lines.append('SaveOutput\n')
@@ -29,6 +38,7 @@ def Generate1InputOpcodeTest(input1: int, output: int):
 
 
 def Generate2InputOpcodeTest(input1: int, input2: int, output: int):
+    global lines
     lines.append(f'set_input {input1}\n')
     lines.append('SaveInput1\n')
     lines.append(f'set_input {input2}\n')
@@ -37,7 +47,30 @@ def Generate2InputOpcodeTest(input1: int, input2: int, output: int):
     lines.append(f'expect 0 {output}\n\n')
 
 
+def Generate1InputIntegerOpcodeTest(input1: int, output: tuple[int, int, int]):
+    global lines
+    lines.append(f'set_input {input1}\n')
+    lines.append('SaveInput1\n')
+    lines.append('SaveOutput\n')
+    lines.append(f'expect 0 {output[0]}\n')
+    lines.append(f'expect 2 {output[1]}\n')
+    lines.append(f'expect 3 {output[2]}\n\n')
+
+
+def Generate2InputIntegerOpcodeTest(input1: int, input2: int, output: tuple[int, int, int]):
+    global lines
+    lines.append(f'set_input {input1}\n')
+    lines.append('SaveInput1\n')
+    lines.append(f'set_input {input2}\n')
+    lines.append('SaveInput2\n')
+    lines.append('SaveOutput\n')
+    lines.append(f'expect 0 {output[0]}\n')
+    lines.append(f'expect 2 {output[1]}\n')
+    lines.append(f'expect 3 {output[2]}\n\n')
+
+
 def GenerateBitwiseOpsTest():
+    global lines
     startTime = time.time()
 
     outputFile = 'Bitwise Operations test.txt'
@@ -55,25 +88,63 @@ def GenerateBitwiseOpsTest():
                     ('ROTR', lambda x,y:  MathUtility.ROTR(x, y)),
                     ('ASHR', lambda x,y:  MathUtility.ASHR(x, y))]
 
-    # go through all 11 bitwise operations, and generate random tests
-    for i in range(len(functionList)):
-        lines.append(f'# Opcode {i+1}: {functionList[i][0]}\n')
-        lines.append(f'set_input {i+1}\n')
-        lines.append('SaveOpcode\n')
+    singleInputOperationList = ['NOT']
 
-        for j in range(numTests):
-            num1 = random.randint(minVal, maxVal)
-            if functionList[i][0] in ['SHL', 'SHR', 'ASHR']:
-                num2 = random.randint(-100, 100)   # restricting the range on the second num is much better for testing SHL and SHR
-                                                # Also, negatives are not allowed in python shift operations, but 
-            else:
-                num2 = random.randint(minVal, maxVal)
-            
-            lines.append(f'# test {j+1}\n')
-            if functionList[i][0] == 'NOT':
+    lines.append('# Bitwise Operations\n')
+
+    startOpcode = 1
+    for i in range(len(functionList)):
+        lines.append(f'# Opcode {i + startOpcode}: {functionList[i][0]}\n')
+        lines.append(f'set_input {i+startOpcode}\n')
+        lines.append('SaveOpcode\n\n')
+
+        testCount = 1  # keep track of how many tests we do
+
+        if (functionList[i][0] in singleInputOperationList):
+            # single input ops
+            # do edge cases
+            for j in edgeCaseValues:
+                lines.append(f'# test {testCount}: edge case\n')
+                Generate1InputOpcodeTest(j, functionList[i][1](j))
+                testCount += 1
+
+            # do low-range random cases
+            for j in range(numRandomTests):
+                num1 = random.randint(lowRangeMinVal, lowRangeMaxVal)
+                lines.append(f'# test {testCount}: random low-range\n')
                 Generate1InputOpcodeTest(num1, functionList[i][1](num1))
-            else:
+                testCount += 1
+
+            # do random cases
+            for j in range(numRandomTests):
+                num1 = random.randint(minVal, maxVal) 
+                lines.append(f'# test {testCount}: random\n')
+                Generate1InputOpcodeTest(num1, functionList[i][1](num1))
+                testCount += 1
+        else:
+            # two input ops
+            # do edge cases
+            for j in edgeCaseValues:
+                for k in edgeCaseValues:
+                    lines.append(f'# test {testCount}: edge case\n')
+                    Generate2InputOpcodeTest(j, k, functionList[i][1](j, k))
+                    testCount += 1
+            
+            # do low-range random cases
+            for j in range(numRandomTests):
+                num1 = random.randint(lowRangeMinVal, lowRangeMaxVal)
+                num2 = random.randint(lowRangeMinVal, lowRangeMaxVal)
+                lines.append(f'# test {testCount}: random low-range\n')
                 Generate2InputOpcodeTest(num1, num2, functionList[i][1](num1, num2))
+                testCount += 1
+
+            # do random cases
+            for j in range(numRandomTests):
+                num1 = random.randint(minVal, maxVal)
+                num2 = random.randint(minVal, maxVal)
+                lines.append(f'# test {testCount}: random\n')
+                Generate2InputOpcodeTest(num1, num2, functionList[i][1](num1, num2))
+                testCount += 1
 
     lines.append('1 1 1 1')
 
@@ -83,8 +154,108 @@ def GenerateBitwiseOpsTest():
     print(f'Lab program file generated. Outputted to {outputDir}/{outputFile}')
     print(f'Generation runtime: {time.time() - startTime}')
 
+    # clear lines
+    lines = []
+
+
+def GenerateIntegerOpsTest():
+    global lines
+    startTime = time.time()
+
+    outputFile = 'Integer Operations test.txt'
+
+    # these tests *also* have to worry about the overflow and divideByZero flags. each function returns a tuple with the answer, and the overflow flag, and the divideByZero flag.
+    functionList = [('Negate', MathUtility.IntegerNegate),
+                    ('Unsigned Addition', MathUtility.UnsignedAddition),
+                    ('Signed Addition', MathUtility.SignedAddition),
+                    ('Unsigned Subtraction', MathUtility.UnsignedSubtraction),
+                    ('Signed Subtraction', MathUtility.SignedSubtraction),
+                    ('Unsigned Multiplication', MathUtility.UnsignedMultiplication),
+                    ('Signed Multiplication', MathUtility.SignedMultiplication),
+                    ('Unsigned Integer Division', MathUtility.UnsignedIntegerDivision),
+                    ('Unsigned Integer Modulo', MathUtility.UnsignedIntegerModulo),
+                    ('Signed Integer Division', MathUtility.SignedIntegerDivision),
+                    ('Signed Integer Modulo', MathUtility.SignedIntegerModulo),
+                    ('Unsigned Integer to Float', MathUtility.UnsignedIntegerToFloat),
+                    ('Signed Integer to Float',  MathUtility.SignedIntegerToFloat),
+                    ('Unsigned Integer to Double', MathUtility.UnsignedIntegerToDouble),
+                    ('Signed Integer to Double', MathUtility.SignedIntegerToDouble)
+                    ]
+    
+    singleInputOperationList = ['Negate', 'Unsigned Integer to Float', 'Signed Integer to Float', 'Unsigned Integer to Double', 'Signed Integer to Double']
+
+    lines.append('# Integer Operations\n')
+
+    # integer ops take up opcodes from 16-30 inclusive
+    startOpcode = 16
+    for i in range(len(functionList)):
+        lines.append(f'# Opcode {i+startOpcode}: {functionList[i][0]}\n')
+        lines.append(f'set_input {i+startOpcode}\n')
+        lines.append('SaveOpcode\n\n')
+
+        testCount = 1  # keep track of how many tests we do
+
+        if (functionList[i][0] in singleInputOperationList):
+            # single input ops
+            # do edge cases
+            for j in edgeCaseValues:
+                lines.append(f'# test {testCount}: edge case\n')
+                Generate1InputIntegerOpcodeTest(j, functionList[i][1](j))
+                testCount += 1
+
+            # do low-range random cases
+            for j in range(numRandomTests):
+                num1 = random.randint(lowRangeMinVal, lowRangeMaxVal)
+                lines.append(f'# test {testCount}: random low-range\n')
+                Generate1InputIntegerOpcodeTest(num1, functionList[i][1](num1))
+                testCount += 1
+
+            # do random cases
+            for j in range(numRandomTests):
+                num1 = random.randint(minVal, maxVal) 
+                lines.append(f'# test {testCount}: random\n')
+                Generate1InputIntegerOpcodeTest(num1, functionList[i][1](num1))
+                testCount += 1
+        else:
+            # two input ops
+            # do edge cases
+            for j in edgeCaseValues:
+                for k in edgeCaseValues:
+                    lines.append(f'# test {testCount}: edge case\n')
+                    Generate2InputIntegerOpcodeTest(j, k, functionList[i][1](j, k))
+                    testCount += 1
+            
+            # do low-range random cases
+            for j in range(numRandomTests):
+                num1 = random.randint(lowRangeMinVal, lowRangeMaxVal)
+                num2 = random.randint(lowRangeMinVal, lowRangeMaxVal)
+                lines.append(f'# test {testCount}: random low-range\n')
+                Generate2InputIntegerOpcodeTest(num1, num2, functionList[i][1](num1, num2))
+                testCount += 1
+
+            # do random cases
+            for j in range(numRandomTests):
+                num1 = random.randint(minVal, maxVal)
+                num2 = random.randint(minVal, maxVal)
+                lines.append(f'# test {testCount}: random\n')
+                Generate2InputIntegerOpcodeTest(num1, num2, functionList[i][1](num1, num2))
+                testCount += 1
+    
+    lines.append('1 1 1 1')
+
+    with open(f'{outputDir}/{outputFile}', 'w+') as f:
+        f.writelines(lines)
+
+    print(f'Lab program file generated. Outputted to {outputDir}/{outputFile}')
+    print(f'Generation runtime: {time.time() - startTime}')
+
+    # cleear lines
+    lines = []
+            
 
 GenerateBitwiseOpsTest()
+GenerateIntegerOpsTest()
+
 
 print(f'Total runtime: {time.time() - globalStartTime}')
 
--- ComponentTesting/Utility/MathUtility.py
+++ ComponentTesting/Utility/MathUtility.py
@@ -1,3 +1,5 @@
+import struct  # for float/double <--> integer conversions
+
 # functions to help with performing operations on a list of bits
 
 
@@ -61,6 +63,7 @@ def BitsToSInt(bits: list) -> int:
     return num
 
 
+# bit operations
 def SHL(input1: int, input2: int) -> int:
     if abs(input2) >= 64:
         return 0
@@ -120,3 +123,135 @@ def ASHR(input1: int, input2: int) -> int:
         
         ans += l1[:64-input2]
         return BitsToSInt(ans)
+
+
+
+# integer operations - outputs are (output, overflow flag, divideByZero flag)
+def IntegerNegate(input1: int) -> tuple[int, int, int]:
+    if input1 == -9223372036854775808:
+        return (input1, 1, 0)
+    return (-input1, 0, 0)
+
+
+def UnsignedAddition(input1: int, input2: int) -> tuple[int, int, int]:
+    # inputs are signed, we need to convert them to unsigned.
+    i1 = BitsToUInt(SIntToBits(input1))
+    i2 = BitsToUInt(SIntToBits(input2))
+    ans = BitsToUInt(IntToBits(i1 + i2))  # keep only the first 64 bits
+    output = BitsToSInt(UIntToBits(ans))  # convert to signed int, because thats how we have to check the output
+
+    if (ans == i1 + i2):  # if these are not equal, then 64 bits is not enough to store the answer, and there is overflow!
+        return (output, 0, 0)
+    return (output, 1, 0)
+
+def SignedAddition(input1: int, input2: int) -> tuple[int, int, int]:
+    ans = BitsToSInt(IntToBits(input1 + input2))  # keep only the first 64 bits
+    
+    if (ans == input1 + input2):  # same idea as in UnsignedAddition()
+        return (ans, 0, 0)
+    return (ans, 1, 0)
+
+def UnsignedSubtraction(input1: int, input2: int) -> tuple[int, int, int]:
+    i1 = BitsToUInt(SIntToBits(input1))
+    i2 = BitsToUInt(SIntToBits(input2))
+    ans = BitsToUInt(IntToBits(i1 - i2))
+    output = BitsToSInt(UIntToBits(ans))
+
+    if (ans == i1 - i2):
+        return (output, 0, 0)
+    return (output, 1, 0)
+
+def SignedSubtraction(input1: int, input2: int) -> tuple[int, int, int]:
+    ans = BitsToSInt(IntToBits(input1 - input2))
+
+    if (ans == input1 - input2):
+        return (ans, 0, 0)
+    return (ans, 1, 0)
+
+def UnsignedMultiplication(input1: int, input2: int) -> tuple[int, int, int]:
+    i1 = BitsToUInt(SIntToBits(input1))
+    i2 = BitsToUInt(SIntToBits(input2))
+    ans = BitsToUInt(IntToBits(i1 * i2))
+    output = BitsToSInt(UIntToBits(ans))
+
+    if (ans == i1 * i2):
+        return (output, 0, 0)
+    return (output, 1, 0)
+
+def SignedMultiplication(input1: int, input2: int) -> tuple[int, int, int]:
+    ans = BitsToSInt(IntToBits(input1 * input2))
+
+    if (ans == input1 * input2):
+        return (ans, 0, 0)
+    return (ans, 1, 0)
+
+def UnsignedIntegerDivision(input1: int, input2: int) -> tuple[int, int, int]:
+    if (input2 == 0):
+        return (0, 0, 1)
+    
+    i1 = BitsToUInt(SIntToBits(input1))
+    i2 = BitsToUInt(SIntToBits(input2))
+    ans = BitsToUInt(IntToBits(i1 // i2))
+    output = BitsToSInt(UIntToBits(ans))
+
+    if (ans == i1 // i2):
+        return (output, 0, 0)
+    return (output, 1, 0)
+
+def SignedIntegerDivision(input1: int, input2: int) -> tuple[int, int, int]:
+    if (input2 == 0):
+        return (0, 0, 1)
+    
+    ans = BitsToSInt(IntToBits(input1 // input2))
+
+    if (ans == input1 // input2):
+        return (ans, 0, 0)
+    return (ans, 1, 0)
+
+def UnsignedIntegerModulo(input1: int, input2: int) -> tuple[int, int, int]:
+    if (input2 == 0):
+        return (0, 0, 1)
+    
+    i1 = BitsToUInt(SIntToBits(input1))
+    i2 = BitsToUInt(SIntToBits(input2))
+    ans = BitsToUInt(IntToBits(i1 % i2))
+    output = BitsToSInt(UIntToBits(ans))
+
+    if (ans == i1 % i2):
+        return (output, 0, 0)
+    return (output, 1, 0)
+
+def SignedIntegerModulo(input1: int, input2: int) -> tuple[int, int, int]:
+    if (input2 == 0):
+        return (0, 0, 1)
+    
+    ans = BitsToSInt(IntToBits(input1 % input2))
+
+    if (ans == input1 % input2):
+        return (ans, 0, 0)
+    return (ans, 1, 0)
+
+
+# returns 64 bits, but only the last 32 bits will ever be nonzero
+def floatToBits(input: float) -> list[int]:
+    return [0] * 32 + SIntToBits(struct.unpack('>l', struct.pack('>f', input))[0])[32:]
+
+def SignedIntegerToFloat(input1: int) -> tuple[int, int, int]:
+    return (BitsToSInt(floatToBits(input1)), 0, 0)
+
+def UnsignedIntegerToFloat(input1: int) -> tuple[int, int, int]:
+    return (BitsToSInt(floatToBits(BitsToUInt(SIntToBits(input1)))), 0, 0)
+
+def doubleToBits(input: float) -> list[int]:
+    return SIntToBits(struct.unpack('>q', struct.pack('>d', input))[0])
+
+def SignedIntegerToDouble(input1: int) -> tuple[int, int, int]:
+    return (BitsToSInt(doubleToBits(input1)), 0, 0)
+
+def UnsignedIntegerToDouble(input1: int) -> tuple[int, int, int]:
+    return (BitsToSInt(doubleToBits(BitsToUInt(SIntToBits(input1)))), 0, 0)
+
+# print(hex(UnsignedIntegerToFloat(2**63-1)[0]))
+#print(hex(BitsToSInt(floatToBits(BitsToUInt(SIntToBits(-1))))))
+#print(BitsToUInt(SIntToBits(1))    )
+# print(SIntToBits(SignedIntegerToFloat(-1)[0]))
